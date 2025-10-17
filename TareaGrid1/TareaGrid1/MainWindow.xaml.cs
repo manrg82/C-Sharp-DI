ce5f8dd96996ce78cc7103c5608a36b52cd6c97d
Manuel Ruiz Gutiérrez <manuelruizgutierrez810@gmail.com>
2025-10-17 12:53:19 +0200

    clase 17/10/2025
    ejs grid

---
+++ TareaGrid1/TareaGrid1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TareaGrid1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            fillGrid();
+        }
+        public void fillGrid()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    Label txt = new Label();
+                    txt.Content = j+"|"+i;
+                    txt.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    txt.VerticalAlignment = VerticalAlignment.Stretch;
+                    Grid.SetRow(txt, i);
+                    Grid.SetColumn(txt, j);
+                    GridTable.Children.Add(txt);
+                    if (j % 2 == 0&& i%2==0|| j % 2 != 0 && i%2!=0)
+                    {
+                        txt.Background = Brushes.Red;
+                    }
+                }
+            }
+        }
+    }
+}
