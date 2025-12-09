f235828ab81ce2687aad01620ce8e6c623b83d8f
Manuel Ruiz Gutiérrez <manuelruizgutierrez810@gmail.com>
2025-12-09 10:33:48 +0100

    clase 09/12/2025

---
+++ ExampleMVCJson/ExampleMVCJson/view/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExampleMVCJson
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Agrega este método en la clase MainWindow
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+            // Lógica para el botón "NEW"
+        }
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            // Lógica para el botón "NEW"
+        }
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Lógica para el botón "NEW"
+        }
+        private void DgvPeople_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            // Aquí puedes agregar la lógica que desees ejecutar cuando cambie la selección
+        }
+    }
+}
