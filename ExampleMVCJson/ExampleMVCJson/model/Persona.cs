ac0c099879de9de07e1df7db51e89a3729440961
Manuel Ruiz Gutiérrez <manuelruizgutierrez810@gmail.com>
2025-12-16 09:31:21 +0100

    hecha tarea json

---
+++ ExampleMVCJson/ExampleMVCJson/model/Persona.cs
@@ -0,0 +1,9 @@
+namespace ExampleMVCJson.model
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
